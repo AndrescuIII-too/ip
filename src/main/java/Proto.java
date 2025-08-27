16460cb722723b15951d04f51f8cc312b4beff9f
AndrescuIII-too <andrescuiii45@gmail.com>
2025-08-28 00:37:12 +0800

    Add initial code

---
+++ src/main/java/Proto.java
@@ -0,0 +1,13 @@
+public class Proto {
+    static String LINE_SEPARATOR = "-".repeat(40);
+
+    public static void main(String[] args) {
+        System.out.println(LINE_SEPARATOR + "\n" +
+                "Hello! I'm Proto\n" +
+                "What can I do for you?\n" +
+                LINE_SEPARATOR + "\n" +
+                "Bye. Hope to see you again soon!\n" +
+                LINE_SEPARATOR
+        );
+    }
+}
