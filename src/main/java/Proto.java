73eee0c667ed23b2eba6cbc6d643bc1578c5ac80
AndrescuIII-too <andrescuiii45@gmail.com>
2025-08-28 00:58:10 +0800

    Add echo on command and exit on bye

--- src/main/java/Proto.java
+++ src/main/java/Proto.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Proto {
     static String LINE_SEPARATOR = "-".repeat(40);
 
@@ -5,9 +8,22 @@ public class Proto {
         System.out.println(LINE_SEPARATOR + "\n" +
                 "Hello! I'm Proto\n" +
                 "What can I do for you?\n" +
-                LINE_SEPARATOR + "\n" +
-                "Bye. Hope to see you again soon!\n" +
                 LINE_SEPARATOR
         );
+
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            String input = reader.nextLine();
+            System.out.println(LINE_SEPARATOR);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n" + LINE_SEPARATOR);
+                break;
+            } else {
+                System.out.println(input + "\n" + LINE_SEPARATOR);
+            }
+        }
     }
 }
