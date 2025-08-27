b7c6649141cafd6a5ee87efc2df9a21d95054be8
AndrescuIII-too <andrescuiii45@gmail.com>
2025-08-28 01:37:07 +0800

    Add ability to store input and list

--- src/main/java/Proto.java
+++ src/main/java/Proto.java
@@ -1,8 +1,9 @@
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Proto {
     static String LINE_SEPARATOR = "-".repeat(40);
+    static ArrayList<String> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(LINE_SEPARATOR + "\n" +
@@ -18,11 +19,20 @@ public class Proto {
             String input = reader.nextLine();
             System.out.println(LINE_SEPARATOR);
 
-            if (input.equals("bye")) {
+            switch (input) {
+            case "bye":
                 System.out.println("Bye. Hope to see you again soon!\n" + LINE_SEPARATOR);
+                return;
+            case "list":
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < taskList.size(); i++) {
+                    sb.append(i + 1).append(". ").append(taskList.get(i)).append("\n");
+                }
+                System.out.println(sb.append(LINE_SEPARATOR));
                 break;
-            } else {
-                System.out.println(input + "\n" + LINE_SEPARATOR);
+            default:
+                taskList.add(input);
+                System.out.println("added: " + input + "\n" + LINE_SEPARATOR);
             }
         }
     }
