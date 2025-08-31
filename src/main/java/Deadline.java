8402dfb7f8f68413301288385b8f454c5dd3f4b1
AndrescuIII-too <andrescuiii45@gmail.com>
2025-08-31 20:57:24 +0800

    Handle dates and times as LocalDate instead of String

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,14 @@
 package main.java;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Deadline extends Task {
-    private final String endTime;
+    private final LocalDate endTime;
 
-    public Deadline(String description, String endTime) {
+    public Deadline(String description, LocalDate endTime) {
         super(description);
         this.endTime = endTime;
     }
@@ -19,24 +21,30 @@ public class Deadline extends Task {
     @Override
     public String getDisplayString() {
         return super.getDisplayString() +
-                " (by: " + this.endTime + ")";
+                " (by: " + this.getEndTimeString() + ")";
+    }
+
+    public String getEndTimeString() {
+        return this.endTime.format(Task.dateTimeFormatter);
     }
 
     public String serialize() {
         return this.getTaskIcon() + " | " + (this.isDone ? "1" : "0") + " | " +
                 Task.encodeString(this.description) + " | " +
-                Task.encodeString(this.endTime);
+                this.getEndTimeString();
     }
 
     private static final Pattern rx_deserialize = Pattern.compile(
             "^D \\| ([01]) \\| ((?:[^|]|\\\\\\|)*) \\| ((?:[^|]|\\\\\\|)*)$");
 
 
-    public static Deadline deserialize(String data) throws ProtoInvalidData {
+    public static Deadline deserialize(String data) throws ProtoInvalidData, DateTimeParseException {
         Matcher match = rx_deserialize.matcher(data);
 
         if (match.find()) {
-            Deadline deadline = new Deadline(Task.decodeString(match.group(2)), Task.decodeString(match.group(3)));
+            Deadline deadline = new Deadline(
+                    Task.decodeString(match.group(2)),
+                    LocalDate.parse(match.group(3), Task.dateTimeFormatter));
             deadline.isDone = match.group(1).equals("1");
             return deadline;
         } else {
